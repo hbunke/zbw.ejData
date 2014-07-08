97e7f664bd580935b99ea16ceb63049b000256d4
Hendrik Bunke <h.bunke@zbw.eu>
2014-07-08 16:00:36 +0200

    init

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='zbw.ejData',
+      version=version,
+      description="collects and displays article level (meta-)data",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Hendrik Bunke',
+      author_email='h.bunke@zbw.eu',
+      url='http://zbw.eu'',
+      license='BSD',
+      packages=find_packages(),
+     # package_dir = {'': 'src'},
+      namespace_packages=['zbw'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
